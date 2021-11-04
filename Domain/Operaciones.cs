5fcd0a91e633688cf8aab5f9954c38c2bb0810a2
JasserRomero247 <jasseromero08@gmail.com>
2021-11-03 21:25:05 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Operaciones.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Operaciones
+    {
+        public int Numero { get; set; }
+        public decimal Valor { get; set; }
+        public TipoGrado TipoGrado { get; set; }
+    }
+}
---
+++ Infraestructure/Formulas.cs
@@ -0,0 +1,57 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure
+{
+    public class Formulas
+    {
+        private List<Operaciones> operaciones;
+
+        public Formulas(List<Operaciones> operaciones)
+        {
+            this.operaciones = operaciones;
+        }
+
+        public void Agregar(Operaciones op)
+        {
+            operaciones.Add(op);
+        }
+
+        public List<Operaciones> Mostrar()
+        {
+            return operaciones;
+        }
+
+        public decimal FhtToCls(decimal valor)
+        {
+            return valor;
+        }
+
+        public decimal ClsToFht(decimal valor)
+        {
+            return valor;
+        }
+
+        public decimal FhtToKvn(decimal valor)
+        {
+            return valor;
+        }
+
+        public decimal KvnToFht(decimal valor)
+        {
+            return valor;
+        }
+
+        public decimal ClsToKvn(decimal valor)
+        {
+            return valor;
+        }
+
+        public decimal KvnToCls(decimal valor)
+        {
+            return valor;
+        }
+    }
+}
